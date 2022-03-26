b14a73010fff5d1d73012e0f48a7f2c1ed7e5c13
Brandon Jones <brandonijones@outlook.com>
2022-03-26 11:29:42 -0400

    Improve AI navigation. Now it avoids obstacles in the way when patroling

--- Food_Freedom_Frenzy/Assets/Scripts/HumanAi.cs
+++ Food_Freedom_Frenzy/Assets/Scripts/HumanAi.cs
@@ -5,70 +5,88 @@ using UnityEngine.AI;
 
 public class HumanAi : MonoBehaviour
 {
-    [Header("Set Dynamically")]
-    public float speed;
-    public Transform target;
-    public float killRange;
-    public float visionDistance;
+    [Header("Set in Inspector")]
+    
+    public NavMeshAgent agent;
+    public Transform player;
+    public Transform startPosition;
 
-    // Patrol specific
     [Header("Patrol Settings")]
+    public float speed;
+    public float chaseAcceleration;
+    public float maxWaitingTime;
+    public float currentWaitingTime;
+    public float sightRange;
+    public float attackRange;
     public Transform[] patrolPoints;
-    public float waitTime;
-
-    private int _currentPointIndex = 0;
+    private int _randomSpot;
+    
+    
+    private void Awake() 
+    {
+        player = GameObject.Find("TempPlayer").transform;
+        startPosition = GameObject.Find("StartPosition").transform;
+        agent = GetComponent<NavMeshAgent>();
+        speed = agent.speed;
+        _randomSpot = Random.Range(0, patrolPoints.Length);
+        
+    }
     
     private void Update() 
     {
-       RaycastHit hitInfo;
+        RaycastHit hit;
 
-       if (Physics.Raycast(transform.position, transform.forward, out hitInfo, visionDistance)) {
-            if (hitInfo.collider.tag == "TempPlayer") {
-                Debug.DrawLine(transform.position, hitInfo.point, Color.red);
-                Chase(hitInfo);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, sightRange)) {
+            if (hit.collider.tag == "TempPlayer") {
+                Chase(hit);
+            } else {
+                Patrol();
             }
-              
-       } else {
+        } else {
             Patrol();
-            Debug.DrawLine(transform.position, transform.position + transform.forward * visionDistance, Color.green);
-       }
+        }
     }
 
     private void Patrol()
     {
-        if (transform.position != patrolPoints[_currentPointIndex].position) {
-            transform.LookAt(patrolPoints[_currentPointIndex]);
-            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[_currentPointIndex].position, speed * Time.deltaTime);
-            if (transform.position == patrolPoints[_currentPointIndex].position) {
-                if (_currentPointIndex == patrolPoints.Length - 1) {
-                    _currentPointIndex = 0;
-                } else {
-                    _currentPointIndex++;
-                }
-                
-            }
-        } 
-    }
 
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(waitTime);
-        if (_currentPointIndex + 1 < patrolPoints.Length) {
-            _currentPointIndex++;
-        } else {
-            _currentPointIndex = 0;
+        // agent.destination = patrolPoints[_randomSpot].position;
+        Debug.DrawLine(transform.position, transform.position + transform.forward * sightRange, Color.green);
+        if (agent.remainingDistance < 0.5f) {
+            if (maxWaitingTime == 0)
+            {
+                maxWaitingTime = Random.Range(2, 5);
+            }
+
+            if (currentWaitingTime >= maxWaitingTime)
+            {
+                maxWaitingTime = 0;
+                currentWaitingTime = 0;
+                _randomSpot = Random.Range(0, patrolPoints.Length);
+                agent.SetDestination(patrolPoints[_randomSpot].position);
+            } else {
+                currentWaitingTime += Time.deltaTime;
+            }
         }
     }
 
-   private void Chase(RaycastHit hitInfo)
+   private void Chase(RaycastHit hit)
    {
-        transform.LookAt(target);
-        if (Vector3.Distance(transform.position, target.position) > killRange && Vector3.Distance(transform.position, target.position) < visionDistance) {
-            transform.position = Vector3.MoveTowards(transform.position, hitInfo.point, speed * Time.deltaTime * 1.25f); 
-            if (Vector3.Distance(transform.position, target.position) <= killRange) {
-                // Debug.DrawLine(transform.position, hitInfo.point, Color.red);
-                Destroy(target.gameObject);
-            }
+        Debug.DrawLine(transform.position, hit.point, Color.red);
+        transform.LookAt(player);
+        if (Vector3.Distance(transform.position, player.position) > attackRange && Vector3.Distance(transform.position, player.position) < sightRange)
+        {
+            // transform.position = Vector3.MoveTowards(transform.position, hit.point, speed * Time.deltaTime * 1.1f);
+            agent.SetDestination(player.position);
+            agent.speed = speed * chaseAcceleration;
+            
+        } 
+
+        if (Vector3.Distance(transform.position, player.position) <= attackRange)
+        {
+            // Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            // Destroy(player.gameObject);
+            player.position = startPosition.position;
         }
-   }  
+   }
 }
