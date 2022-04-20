5e6f1d20f7371a61208064f4d05cbf5e0e0acf9d
JosephTech <49079887+JosephTech@users.noreply.github.com>
2022-04-20 00:52:20 -0400

    fix: fix merged conflicts

--- Food_Freedom_Frenzy/Assets/Scripts/Menu/GameOver.cs
+++ Food_Freedom_Frenzy/Assets/Scripts/Menu/GameOver.cs
@@ -7,12 +7,7 @@ public class GameOver : MonoBehaviour
 {
     public void RestartLevelOne()
     {
-<<<<<<< HEAD:Food_Freedom_Frenzy/Assets/Scripts/Menu/GameOver.cs
         SceneManager.LoadScene("Level_1");
-=======
-        // SceneManager.LoadScene("Release_2");
-        SceneManager.LoadScene("Release_2_COPY");
->>>>>>> 9474859ed1ea129da68ab71e38a3a6ea9edcac1b:Food_Freedom_Frenzy/Assets/Scripts/GameOver.cs
     }
 
     public void RestartLevelTwo()
--- Food_Freedom_Frenzy/Assets/Scripts/Menu/MainMenu.cs
+++ Food_Freedom_Frenzy/Assets/Scripts/Menu/MainMenu.cs
@@ -5,17 +5,12 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-<<<<<<< HEAD:Food_Freedom_Frenzy/Assets/Scripts/Menu/MainMenu.cs
         SceneManager.LoadScene("Level_1");
     }
 
     public void Tutorial()
     {
         SceneManager.LoadScene("Tutorial");
-=======
-        // SceneManager.LoadScene("Release_2");
-        SceneManager.LoadScene("Release_2_COPY");
->>>>>>> 9474859ed1ea129da68ab71e38a3a6ea9edcac1b:Food_Freedom_Frenzy/Assets/Scripts/MainMenu.cs
     }
 
     public void ExitGame()
